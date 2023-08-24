2694f06edb5acd84477484ea0d7341f36944dfab
yavuzyarkinokular <yarkinokular@gmail.com>
2023-08-24 18:48:03 +0300

    file name and path changed

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRoutes = require("./routes/indexRoutes");
+var usersRoutes = require("./routes/usersRoutes");
 
 const session = require("express-session");
 
@@ -29,8 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", indexRoutes);
+app.use("/users", usersRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
